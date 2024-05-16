dc3ff9d11a841a75fa1f81393cf4b684299698da
ErinyAnis <erinyanis88@gmail.com>
2024-05-16 15:57:04 +0300

    eriny

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ $(document).ready(function () {
     if (scroll >= 50) {
       $("header nav").addClass("fixed-top");
       $("header nav").addClass("white-background");
+      $("header nav .navbar-toggler .toggle-icon").css("color", "#1D1B1B");
     } else {
       $("header nav").removeClass("fixed-top");
       $("header nav").removeClass("white-background");
+      $("header nav .navbar-toggler .toggle-icon").css("color", "#FEFEFE");
     }
   });
 });
